eba0611b14ce7d871ef6fe1ccb90fc49d81499aa
masterhafizibrahim-sys <masterhafizibrahim@gmail.com>
2026-09-02 21:45:51 +1000

    server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/download", async (req, res) => {
 
     const outputPath = path.join(__dirname, "video.mp4");
 
-    const ytdlp = new YtDlpWrap();
+    const ytdlp = new YtDlpWrap("/usr/local/bin/yt-dlp");
 
     // ⭐ CLOUD-SAFE YT-DLP FLAGS
     await ytdlp.exec([
